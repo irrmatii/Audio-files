302be0a6b480a54dcec8322d314710b0603cf80e
irrmatii <itadserving@gmail.com>
2024-12-06 10:37:34 +0200

    first commit

---
+++ script.ts
@@ -0,0 +1,30 @@
+const playBtn = document.querySelectorAll("svg") as NodeListOf<SVGElement>;
+const audio = document.querySelectorAll("audio");
+
+
+
+playBtn[0].onclick = () => {
+    console.log("clicked")
+    audio[0].play()
+    audio[1].pause()
+    audio[2].pause()
+}
+
+playBtn[0].ondblclick = () => {
+    console.log("DBLclicked")
+    audio[0].pause()
+}
+
+playBtn[1].onclick = () => {
+    console.log("clicked")
+    audio[1].play()
+    audio[0].pause()
+    audio[2].pause()
+}
+
+playBtn[2].onclick = () => {
+    console.log("clicked")
+    audio[2].play()
+    audio[0].pause()
+    audio[1].pause()
+}
